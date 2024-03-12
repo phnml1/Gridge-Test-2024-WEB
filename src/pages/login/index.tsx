bf21d31f767672e28e9a9d8fde40e342b035e6ba
lee juyoung <juyung0903@naver.com>
2024-03-12 12:01:34 +0900

    design: 로그인페이지의 LoginBox 디자인
    LoginBox는 아이디, 패스워드 input창과 로그인버튼 및 카카오로그인, 비밀번호를 잊으셨나요 버튼 포함

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,68 +1,74 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { InputWrap, LoginButton, LoginRoot, Wrap } from "./styles";
-import { useRecoilState } from "recoil";
-import { jwtState, nameState } from "../../recoil/login";
-
+// import { useNavigate } from "react-router-dom";
+import { InputWrap,IphonesWrap, LoginButton, LoginRoot, LoginWrap, Logo, Wrap, KakaoButton, LoginBox, Or, ForgetPassword } from "./styles";
+import LogoImage from '../../assets/inobel-logo.png'
+import Input from '../../components/Input';
+import mail from '../../assets/mail.png';
+import lock from '../../assets/lock.png';
+import kakaologo from '../../assets/kakaologo.png';
+// import { useRecoilState } from "recoil";
+// import { jwtState, nameState } from "../../recoil/login";
 const Login = () => {
-  const navigate = useNavigate();
-  const [name, setName] = useRecoilState(nameState);
-  const [, setJwt] = useRecoilState(jwtState);
+  // const navigate = useNavigate();
+  // const [name, setName] = useRecoilState(nameState);
+  // const [, setJwt] = useRecoilState(jwtState);
 
-  // Input 에서 엔터키 누를 경우
-  const onKeyUp = (event: React.KeyboardEvent<HTMLElement>) => {
-    const key = event.key || event.keyCode;
-    if (key === "Enter" || key === 13) {
-      handleLogin().then();
-    }
-  };
+  // // Input 에서 엔터키 누를 경우
+  // const onKeyUp = (event: React.KeyboardEvent<HTMLElement>) => {
+  //   const key = event.key || event.keyCode;
+  //   if (key === "Enter" || key === 13) {
+  //     handleLogin().then();
+  //   }
+  // };
 
-  // 로그인 버튼 클릭
-  const handleLogin = async () => {
-    try {
-      if (Object.keys(name).length === 0) {
-        alert("이름을 입력해 주세요.");
-        return;
-      }
+  // // 로그인 버튼 클릭
+  // const handleLogin = async () => {
+  //   try {
+  //     if (Object.keys(name).length === 0) {
+  //       alert("이름을 입력해 주세요.");
+  //       return;
+  //     }
 
-      //서버통신 코드 작성
+  //     //서버통신 코드 작성
 
-      setJwt("success login");
+  //     setJwt("success login");
 
-      navigate(`/`);
-    } catch (error) {
-      alert("네트워크 통신 실패. 잠시후 다시 시도해주세요.");
-    }
-  };
+  //     navigate(`/`);
+  //   } catch (error) {
+  //     alert("네트워크 통신 실패. 잠시후 다시 시도해주세요.");
+  //   }
+  // };
 
   return (
     <LoginRoot>
       <Wrap>
-        <div
-          style={{
-            fontSize: "2.5rem",
-            fontWeight: "600",
-            marginBottom: "4rem",
-          }}
-        >
-          로그인
-        </div>
-        <div
-          style={{
-            fontSize: "1.8rem",
-            fontWeight: "600",
-            marginBottom: "1rem",
-          }}
-        >
-          이름
-        </div>
-        <InputWrap
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="이름을 입력해주세요."
-          onKeyUp={onKeyUp}
-        />
-        <LoginButton onClick={handleLogin}>로그인</LoginButton>
+        <IphonesWrap>
+
+        </IphonesWrap>
+        <LoginWrap>
+          <LoginBox>
+            <Logo src={LogoImage} alt="logo image"/>
+            <InputWrap>
+            <Input image={mail} placeholder="전화번호,사용자 이름 또는 이메일"/>
+            <Input image = {lock} placeholder="비밀번호"/>
+            <LoginButton>
+              로그인
+              </LoginButton>
+              <Or>
+                or
+              </Or>
+              <KakaoButton>
+                <img src={kakaologo} width={22} height={22} alt="카카오 버튼"/>
+                카카오 로그인
+              </KakaoButton>
+              <ForgetPassword>
+              비밀번호를 잊으셨나요?
+            </ForgetPassword>
+            </InputWrap>
+            
+          </LoginBox>
+        
+        </LoginWrap>
       </Wrap>
     </LoginRoot>
   );
--- src/pages/login/styles.tsx
+++ src/pages/login/styles.tsx
@@ -1,51 +1,121 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { supportDeviceSize } from "../../components/styles";
 
 export const LoginRoot = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 1080px;
-  height: 100vh;
-  background-color: green;
+  width: 1440px;
+  height: 1024px;
+  background-color: #fff;
 
-  @media all and (max-width: ${supportDeviceSize}px) {
-    width: 100vw;
-  }
+  // @media all and (max-width: ${supportDeviceSize}px) {
+  //   width: 100vw;
+  // }
 `;
 
+export const Font = css`
+  font-family: SF Pro Display;
+  line-height: 24px;
+  letter-spacing: 0em;
+  font-size: 16px;
+`;
 export const Wrap = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: start;
+  align-items: center;
   justify-content: center;
-  width: 45rem;
-  height: 30rem;
-  padding: 3rem;
-  border: 0.5rem solid yellow;
-  background-color: red;
+  width: auto;
+  height: 780px;
+  gap: 72px;
 `;
 
-export const InputWrap = styled.input`
+export const IphonesWrap = styled.div`
+  width: 520px;
+  height: 100%;
+`;
+
+export const LoginWrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+  width: 416px;
+  height: 100%;
+`;
+export const LoginBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
   width: 100%;
-  height: 4rem;
-  padding-left: 1rem;
-  margin-bottom: 6rem;
+  height: 559px;
+  border: 1px solid #cccccc;
+  position: relative;
+`;
+export const LoginButton = styled.div`
+  width: 320px;
+  padding: 10px 0;
+  border-radius: 30px;
+  ${Font}
+  font-weight:600;
+  text-align: center;
+  color: #ffffff;
+  cursor: pointer;
+  margin-top: 10px;
+  background: #b2ddff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-export const LoginButton = styled.div`
+export const Logo = styled.img`
+  width: 217px;
+  height: 80px;
+  margin-top: 70px;
+`;
+export const InputWrap = styled.div`
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  position: absolute;
+  top: 207px;
+`;
+export const Or = styled.div`
+width: 14px;
+height: 24px;
+${Font}
+font-size:16px;
+color: #7F7F7F;
+display:flex;
+align-itmes: center;
+`;
+export const KakaoButton = styled.button`
   width: 100%;
-  padding: 1rem;
-  border-radius: 0.6rem;
-  background-color: rgb(59, 105, 246);
-  font-size: 1.8rem;
-  font-weight: 600;
-  color: white;
+  border-radius: 30px;
   text-align: center;
   cursor: pointer;
+  background: #f8d706;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0;
+  gap: 2px;
+  ${Font}
+  font-weight: 600;
+`;
+export const ForgetPassword = styled.div`
+  font-family: SF Pro Display;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0em;
+  text-align: center;
+  margin-top: 20px;
+  height:20px;
+  color: #7f7f7f;
+  margin-bottom: 40px;
+  cursor:pointer;
 `;
