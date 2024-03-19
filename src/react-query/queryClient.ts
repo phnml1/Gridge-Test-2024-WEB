f04094968f6b906b5397e6a14f12ad88b34b591e
lee juyoung <juyung0903@naver.com>
2024-03-19 13:27:51 +0900

    setting: react-query

---
+++ src/react-query/queryClient.ts
@@ -0,0 +1,27 @@
+import { MutationCache, QueryCache, QueryClient } from 'react-query';
+
+function queryErrorHandler(error: unknown): void {
+//   // error is type unknown because in js, anything can be an error (e.g. throw(5))
+//   const title =
+//     error instanceof Error ? error.message : 'error connecting to server';
+console.log(error);
+}
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: queryErrorHandler,
+  }),
+  mutationCache: new MutationCache({
+    onError: queryErrorHandler,
+  }),
+  defaultOptions: {
+    queries: {
+      onError: queryErrorHandler,
+      staleTime: 3000,
+      cacheTime: 900000,
+      refetchOnMount: false,//쿼리 unmount될때 inactive로 됨
+      refetchOnReconnect: false, //네트워크 재연결시 다시?
+      refetchOnWindowFocus: false, //창에 초점다시 맞출때 다시?
+    },
+  },
+});
